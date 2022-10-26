8c7084527a824b2acfb225605e9d1a1e40754f40
natacha <nat_bartu@hotmail.com>
2022-10-26 14:33:27 +1000

    create a card-container components and add button structure into the component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from "react";
-import Container from "./components/Container";
+import React, { useState } from "react";
+import CardContainer from "./components/CardContainer";
 
 const App = () => {
+  const [images, setImages] = useState([
+    { url: "https://source.unsplash.com/random/1" },
+    { url: "https://source.unsplash.com/random/2" },
+    { url: "https://source.unsplash.com/random/3" },
+    { url: "https://source.unsplash.com/random/4" },
+    { url: "https://source.unsplash.com/random/5" },
+    { url: "https://source.unsplash.com/random/6" },
+  ]);
+
   return (
     <div className="App">
-      <Container></Container>
+      <CardContainer />
     </div>
   );
 };
---
+++ src/components/CardContainer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./CardContainer.css";
+
+const CardContainer = () => {
+  return (
+    <div className="card-container">
+      <div className="btnContainer">
+        <button>Remove</button>
+        <button>Add</button>
+      </div>
+    </div>
+  );
+};
+
+export default CardContainer;
