d4f971cf164251a0703c2c70826417fd004e5280
natacha <nat_bartu@hotmail.com>
2022-10-26 14:16:45 +1000

    add components folder and create a container components, pass children into thecontainer components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Container from "./components/Container";
 
 const App = () => {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Container></Container>
+    </div>
+  );
 };
 
 export default App;
