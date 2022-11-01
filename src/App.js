86f757586c8ca5630336436fa1d7f49e09550967
natacha <nat_bartu@hotmail.com>
2022-11-01 11:38:47 +1000

    remove Images Container, pass images state into Card Components and render images through mapping over images

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import Container from "./components/container/Container";
-import Images from "./components/images/Images";
 
 const App = ({ children }) => {
   return (
     <div className="App">
       {children}
-      <Images />
       <Container />
     </div>
   );
--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Card.css";
 
 const Card = () => {
-  return <div className="card-container">Card Container</div>;
+  const [images, setImages] = useState([
+    { url: "https://source.unsplash.com/random/1" },
+    { url: "https://source.unsplash.com/random/2" },
+    { url: "https://source.unsplash.com/random/3" },
+    { url: "https://source.unsplash.com/random/4" },
+    { url: "https://source.unsplash.com/random/5" },
+    { url: "https://source.unsplash.com/random/6" },
+  ]);
+
+  return (
+    <div className="CardContainer">
+      {images.map((img, idx) => (
+        <div
+          className="card"
+          key={idx}
+          style={{ background: `url("${img.url}") no-repeat center/cover` }}
+        ></div>
+      ))}
+    </div>
+  );
 };
 
 export default Card;
